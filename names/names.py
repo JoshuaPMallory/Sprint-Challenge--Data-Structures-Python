ac5ff49f66ae805dbb71ccec011ffbcc920d65ba
Joshua <56405416+JoshuaPMallory@users.noreply.github.com>
2020-05-15 09:38:40 -0700

    Honestly this one is hacky
    Pandas makes it a lot faster :V

--- names/names.py
+++ names/names.py
@@ -1,28 +1,83 @@
 import time
 
+
+def dup_baseline():
+	f = open('names_1.txt', 'r')
+	names_1 = f.read().split("\n")  # List containing 10000 names
+	f.close()
+
+	f = open('names_2.txt', 'r')
+	names_2 = f.read().split("\n")  # List containing 10000 names
+	f.close()
+
+	duplicates = []  # Return the list of duplicates in this data structure
+
+	# Replace the nested for loops below with your improvements
+	for name_1 in names_1:
+	    for name_2 in names_2:
+	        if name_1 == name_2:
+	            duplicates.append(name_1)
+	return duplicates
+
+def dup_tuple():
+	f = open('names_1.txt', 'r')
+	names_1 = tuple(f.read().split("\n") ) # ~List~ Tuple containing 10000 names
+	f.close()
+
+	f = open('names_2.txt', 'r')
+	names_2 = tuple(f.read().split("\n"))  # ~List~ Tuple containing 10000 names
+	f.close()
+
+	duplicates = []  # Return the list of duplicates in this data structure
+
+	# Replace the nested for loops below with your improvements
+	for name_1 in names_1:
+	    for name_2 in names_2:
+	        if name_1 == name_2:
+	            duplicates.append(name_1)
+	return duplicates
+
+def dup_pd():
+	'''Pandas (Uses C)'''
+	import pandas as pd
+
+
+	s1 = pd.read_csv('names_1.txt', names = ['1'])
+	s2 = pd.read_csv('names_2.txt', names = ['1'])
+
+	df = pd.concat([s1, s2], ignore_index = True)
+
+	duplicates = df[df.duplicated()]['1'].values.tolist()
+
+	return duplicates
+
+def endit(duplicates):
+	end_time = time.time()
+	print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+	print (f"runtime: {end_time - start_time} seconds")
+
+
+print('Baseline Test')
 start_time = time.time()
+endit(dup_baseline())
+print()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+print('Tuple Test')
+start_time = time.time()
+endit(dup_tuple())
+print()
 
-duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+print('Pandas Test')
+start_time = time.time()
+endit(dup_pd())
+print()
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
