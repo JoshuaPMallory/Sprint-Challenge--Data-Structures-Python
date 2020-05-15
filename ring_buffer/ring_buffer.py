7d8716563d3092551f652a7c9a3c7ec6c00481dd
Joshua <56405416+JoshuaPMallory@users.noreply.github.com>
2020-05-15 10:17:50 -0700

    EzPz
    Lemon le Squee-Z

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+	def __init__(self, capacity):
+		self.capacity = int(capacity)
+		self.pos      = 0
+		self.buffer   = []
 
-    def append(self, item):
-        pass
-
-    def get(self):
-        pass
+	def append(self, item):
+		try:
+			self.buffer[self.pos] = item
+		except IndexError:
+			self.buffer.insert(self.pos, item)
+		
+		if self.pos + 1 == self.capacity:
+			self.pos = 0
+		else:
+			self.pos += 1
+		
+	def get(self):
+		return self.buffer
